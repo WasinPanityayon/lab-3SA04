3cda5a955341f9eddc1db073b32a1b5da5776ca8
WasinPanityayon <wasinpanityayon08@gmail.com>
2022-08-27 19:20:19 +0700

    Commit 4

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -12,40 +12,45 @@ const availableZipItems = [
 ]
 
 const ZipItem = ({place, code, navigation}) => (
-    <TouchableHighlight onPress={() => navigation.navigate('Weather', { zipCode: code})}>
+    <TouchableHighlight onPress={() => {
+        navigation.navigate('Weather', {zipCode: code})
+    }}>
         <View style={styles.zipItem}>
-            <Text>{place}</Text>
-            <Text>{code}</Text>
+            <Text style={styles.zipPlace}>{place}</Text>
+            <Text style={styles.zipCode}>{code}</Text>
         </View>
     </TouchableHighlight>
 )
 
-const _keyExtractor = item => item.code
-
-export default function ZipCodeScreen(){
+export default function ZipCodeScreen() {
     const navigation = useNavigation()
     return (
-        <View>
         <FlatList
-            data={availableZipItems}
-            keyExtractor={_keyExtractor}
-            renderItem={({item}) => <ZipItem {...item} navigation={navigation}/>}
-            />
-            <StatusBar style="auto" />
-        </View>
-    ); 
+            data = {availableZipItems}
+            keyExtractor = {item => item.code}
+            renderItem = {({item}) => <ZipItem {...item} navigation={navigation}/>}
+        />
+    )
 }
 
 const styles = StyleSheet.create({
     zipItem: {
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        backgroundColor: '#8c586f75',
+        padding: 20,
+        borderRadius: 50,
+        marginVertical: 10,
+        marginHorizontal: 30
     },
     zipPlace: {
-        flex: 1,
+        fontSize: 20,
+        paddingLeft: 40
     },
     zipCode: {
-        flex: 1,
+        fontSize: 20,
+        paddingRight: 50,
+        color: '#323232'
     }
 })
